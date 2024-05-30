481f2ba6cacd8f6771a631a189739f39bef02101
cgk95 <kjkj5868@gmail.com>
2024-05-30 17:31:06 +0900

    STACK :: Leet_validParentheses선풀이 개

--- src/main/kotlin/Leet_validParentheses.kt
+++ src/main/kotlin/Leet_validParentheses.kt
@@ -1,41 +1,20 @@
 class Leet_validParentheses {
+
     fun isValid(s: String): Boolean {
-        val stack = ArrayDeque<Char>()
-        for (char in s.toCharArray()) {
-            if (stack.isEmpty()) {
-                if ("([{".contains(char)) {
-                    stack.add(char)
-                    continue
-                }
+        val table: Map<Char, Char> = mapOf(
+            ')' to '(',
+            ']' to '[',
+            '}' to '{',
+        )
+        val stack: ArrayDeque<Char> = ArrayDeque()
+
+        for (i in s.indices) {
+            if (!table.containsKey(s[i])) {
+                stack.addLast(s[i])
+            } else if (stack.isEmpty() || table[s[i]] != stack.removeLast()) {
                 return false
             }
-            if (")]}".contains(char)) {
-                when (char) {
-                    ')' -> if (stack.last() == '(') {
-                        stack.removeLast()
-                        continue
-                    } else {
-                        return false
-                    }
-
-                    ']' -> if (stack.last() == '[') {
-                        stack.removeLast()
-                        continue
-                    } else {
-                        return false
-                    }
-
-                    '}' -> if (stack.last() == '{') {
-                        stack.removeLast()
-                        continue
-                    } else {
-                        return false
-                    }
-                }
-            }
-            stack.add(char)
         }
-
-        return stack.size == 0
+        return stack.isEmpty()
     }
 }
